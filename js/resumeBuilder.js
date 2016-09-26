1a34232a0b8256e2c59fa33476f9e4897e011dc3
Ethan Rogers <erogers6264@gmail.com>
2016-09-26 13:52:21 -0700

    Add skills in the form of a js array

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,10 @@
 var name = "M. Ethan Rogers";
-var role = "developer of the whole stack"
+var role = "Fullstack Web Developer";
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var skills = ["programming", "teaching", "python", "SQL & noSQL", "JS", "CSS3", "HTML5", "responsive design", "Google App Engine"];
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#main").append(skills);
