97826a0dc4e63ff415a1838d5e4987b69f48e865
Ethan Rogers <erogers6264@gmail.com>
2016-09-26 21:32:23 -0700

    Add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,24 @@
 var name = "M. Ethan Rogers";
 var role = "Fullstack Web Developer";
+var contactInfo = {
+	"email": "erogers6264@gmail.com",
+	"phone": "(915) 637-8415"
+};
+var picUrl = "images/me.jpg";
+
+var bio = {
+	"name": name,
+	"role": role,
+	"contactInfo": contactInfo,
+	"picUrl": picUrl
+};
+
+var skills = ["programming", "teaching", "python", "SQL & noSQL", "JS", "CSS3", "HTML5", "responsive design", "Google App Engine"];
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
-var skills = ["programming", "teaching", "python", "SQL & noSQL", "JS", "CSS3", "HTML5", "responsive design", "Google App Engine"];
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#main").append(skills);
+$("#main").append(bio.picUrl);
