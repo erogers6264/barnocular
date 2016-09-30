1c799704234516904f6346ce3786700ca3528c2a
Ethan Rogers <erogers6264@gmail.com>
2016-09-30 14:52:01 -0700

    Clean up code except for resume objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,36 +1,43 @@
-var name = "M. Ethan Rogers";
-var role = "Fullstack Web Developer";
-var contactInfo = {
-	"email": "erogers6264@gmail.com",
-	"phone": "(915) 637-8415"
-};
-var picUrl = "images/me.jpg";
-
 var bio = {
-	"name": name,
-	"role": role,
-	"contactInfo": contactInfo,
-	"picUrl": picUrl
+	"name": "M. Ethan Rogers",
+	"role": "Fullstack Web Developer",
+	"contactInfo": {
+		"email": "erogers6264@gmail.com",
+		"phone": "(915) 637-8415"
+	},
+	"picUrl": "images/me.jpg"
+	"skills" = ["programming", "teaching", "python", "relational databases",
+		"nonrelational databases", "JS", "CSS3", "HTML5", "responsive design",
+		"Google App Engine"];
+};
+
+var projects = {
+	"personal": [
+		{
+		"title": "menumoo"
+		"toolsUsed": ["Flask", "Python", "Postgresql", "SQLAlchemy", "Skeleton"]
+		}
+	]
 };
 
 var education = {
 	"schools": [
 		{
-			"name": "University of Arizona",
+			"schoolName": "University of Arizona",
 			"city": "Tucson, AZ, US",
 			"degree": "Bachelors",
 			"major": ["German Studies", "Linguistics"],
 			"gradYear": "2012"
 		},
 		{
-			"name": "University of Arizona",
+			"schoolName": "University of Arizona",
 			"city": "Tucson, AZ, US",
 			"degree": "Masters",
 			"major": ["German Studies"],
 			"gradYear": "2015"
 		},
 		{
-			"name": "Udacity",
+			"schoolName": "Udacity",
 			"city": "Mountain View, CA, US",
 			"degree": "",
 			"major": ["Fullstack Web Development"],
@@ -39,12 +46,43 @@ var education = {
 	]
 };
 
-var skills = ["programming", "teaching", "python", "SQL & noSQL", "JS", "CSS3", "HTML5", "responsive design", "Google App Engine"];
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-$("#main").append(skills);
-$("#main").append(bio.picUrl);
+var employment = {
+	"workplaces": [
+		{
+		"position": "host",
+		"employer": "Native Grill & Wings",
+		"yearsWorked": 1,
+		"city": "Tucson, AZ, US",
+		"startDate": "September 2016",
+		"endDate": undefined,
+		"supervisorName": "Chad Petri"
+		},
+		{
+		"position": "host",
+		"employer": "Native Grill & Wings",
+		"yearsWorked": 1,
+		"city": "Tucson, AZ, US",
+		"startDate": "September 2016",
+		"endDate": undefined,
+		"supervisorName": "Chad Petri"
+		},
+		{
+		"position": "host",
+		"employer": "Native Grill & Wings",
+		"yearsWorked": 1,
+		"city": "Tucson, AZ, US",
+		"startDate": "September 2016",
+		"endDate": undefined,
+		"supervisorName": "Chad Petri"
+		},
+		{
+		"position": "host",
+		"employer": "Native Grill & Wings",
+		"yearsWorked": 1,
+		"city": "Tucson, AZ, US",
+		"startDate": "September 2016",
+		"endDate": undefined,
+		"supervisorName": "Chad Petri"
+		},
+	]
+};
