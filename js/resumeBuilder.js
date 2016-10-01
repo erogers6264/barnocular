0d7e3dfa7d13a7044ddb2a7d71abb3bc5dc45f8d
Ethan Rogers <erogers6264@gmail.com>
2016-10-01 12:34:25 -0700

    Convert tabs to spaces

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,89 +1,88 @@
 var bio = {
-	"name": "M. Ethan Rogers",
-	"role": "Fullstack Web Developer",
-	"contactInfo": {
-		"email": "erogers6264@gmail.com",
-		"phone": "(915) 637-8415"
-	},
-	"picUrl": "images/me.jpg"
-	"skills" = ["programming", "teaching", "python", "relational databases",
-		"nonrelational databases", "JS", "CSS3", "HTML5", "responsive design",
-		"Google App Engine"];
+    "name": "M. Ethan Rogers",
+    "role": "Fullstack Web Developer",
+    "contactInfo": {
+        "email": "erogers6264@gmail.com",
+        "phone": "(915) 637-8415"
+    },
+    "picUrl": "images/me.jpg",
+    "skills": ["programming", "teaching", "python", "relational databases",
+            "nonrelational databases", "JS", "CSS3", "HTML5", "responsive design",
+            "Google App Engine"]
 };
 
 var projects = {
-	"personal": [
-		{
-		"title": "menumoo"
-		"toolsUsed": ["Flask", "Python", "Postgresql", "SQLAlchemy", "Skeleton"]
-		}
-	]
+    "personal": [
+        {
+            "title": "menumoo",
+            "toolsUsed": ["Flask", "Python", "Postgresql", "SQLAlchemy", "Skeleton"]
+        }
+    ]
 };
 
 var education = {
-	"schools": [
-		{
-			"schoolName": "University of Arizona",
-			"city": "Tucson, AZ, US",
-			"degree": "Bachelors",
-			"major": ["German Studies", "Linguistics"],
-			"gradYear": "2012"
-		},
-		{
-			"schoolName": "University of Arizona",
-			"city": "Tucson, AZ, US",
-			"degree": "Masters",
-			"major": ["German Studies"],
-			"gradYear": "2015"
-		},
-		{
-			"schoolName": "Udacity",
-			"city": "Mountain View, CA, US",
-			"degree": "",
-			"major": ["Fullstack Web Development"],
-			"gradYear": "2016"
-		}
-	]
+    "schools": [
+        {
+            "schoolName": "University of Arizona",
+            "city": "Tucson, AZ, US",
+            "degree": "Bachelors",
+            "major": ["German Studies", "Linguistics"],
+            "gradYear": "2012"
+        },
+        {
+            "schoolName": "University of Arizona",
+            "city": "Tucson, AZ, US",
+            "degree": "Masters",
+            "major": ["German Studies"],
+            "gradYear": "2015"
+        },
+        {
+            "schoolName": "Udacity",
+            "city": "Mountain View, CA, US",
+            "degree": "",
+            "major": ["Fullstack Web Development"],
+            "gradYear": "2016"
+        }
+    ]
 };
 
 var employment = {
-	"workplaces": [
-		{
-			"position": "host",
-			"employer": "Native Grill & Wings",
-			"yearsWorked": 1,
-			"city": "Tucson, AZ, US",
-			"startDate": "September 2016",
-			"endDate": undefined,
-			"supervisorName": "Chad Petri"
-		},
-		{
-			"position": "host",
-			"employer": "Native Grill & Wings",
-			"yearsWorked": 1,
-			"city": "Tucson, AZ, US",
-			"startDate": "September 2016",
-			"endDate": undefined,
-			"supervisorName": "Chad Petri"
-		},
-		{
-			"position": "host",
-			"employer": "Native Grill & Wings",
-			"yearsWorked": 1,
-			"city": "Tucson, AZ, US",
-			"startDate": "September 2016",
-			"endDate": undefined,
-			"supervisorName": "Chad Petri"
-		},
-		{
-			"position": "host",
-			"employer": "Native Grill & Wings",
-			"yearsWorked": 1,
-			"city": "Tucson, AZ, US",
-			"startDate": "September 2016",
-			"endDate": undefined,
-			"supervisorName": "Chad Petri"
-		},
-	]
+    "workplaces": [
+        {
+            "position": "host",
+            "employer": "Native Grill & Wings",
+            "yearsWorked": 1,
+            "city": "Tucson, AZ, US",
+            "startDate": "September 2016",
+            "endDate": undefined,
+            "supervisorName": "Chad Petri"
+        },
+        {
+            "position": "host",
+            "employer": "Native Grill & Wings",
+            "yearsWorked": 1,
+            "city": "Tucson, AZ, US",
+            "startDate": "September 2016",
+            "endDate": undefined,
+            "supervisorName": "Chad Petri"
+        },
+        {
+            "position": "host",
+            "employer": "Native Grill & Wings",
+            "yearsWorked": 1,
+            "city": "Tucson, AZ, US",
+            "startDate": "September 2016",
+            "endDate": undefined,
+            "supervisorName": "Chad Petri"
+        },
+        {
+            "position": "host",
+            "employer": "Native Grill & Wings",
+            "yearsWorked": 1,
+            "city": "Tucson, AZ, US",
+            "startDate": "September 2016",
+            "endDate": undefined,
+            "supervisorName": "Chad Petri"
+        }
+    ]
 };
-
