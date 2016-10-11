15d89d9978c616041425a9f032b2b5fd2bd0ac51
Ethan Rogers <erogers6264@gmail.com>
2016-10-11 11:55:43 -0700

    Uncomment restaurant jobs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -89,13 +89,13 @@ var education = {
 
 var work = {
     "jobs": [
-        // {
-        //     "employer": "Native Grill & Wings",
-        //     "title": "Host & Expo",
-        //     "location": "Tucson, AZ",
-        //     "dates": "in progress",
-        //     "description": "Provide an excellent first impression to guests, explain the daily specials, and perform regular restaurant tasks such as handling to-go orders and cleaning."
-        // },
+        {
+            "employer": "Native Grill & Wings",
+            "title": "Host & Expo",
+            "location": "Tucson, AZ",
+            "dates": "in progress",
+            "description": "Provide an excellent first impression to guests, explain the daily specials, and perform regular restaurant tasks such as handling to-go orders and cleaning."
+        },
         {
             "employer": "Walden Grove High School/Sahuarita School District",
             "title": "Long-term Substitute German Teacher",
@@ -103,13 +103,13 @@ var work = {
             "dates": "2016",
             "description": "Acted as a stand-in instructor for four sections of German 1. Responsible for all planning, instruction and assessment. Showed students that they can also learn outside when the weather permits."
         },
-        // {
-        //     "employer": "PF Changs China Bistro",
-        //     "title": "Server",
-        //     "location": "Tucson, AZ",
-        //     "dates": "2015-2016",
-        //     "description": "Serve Asian-inspired cuisine in a vibrant bistro atmosphere. Welcome and engage guests. Keep things organized and clean. Provide exceptional service to every guest."
-        // },
+        {
+            "employer": "PF Changs China Bistro",
+            "title": "Server",
+            "location": "Tucson, AZ",
+            "dates": "2015-2016",
+            "description": "Serve Asian-inspired cuisine in a vibrant bistro atmosphere. Welcome and engage guests. Keep things organized and clean. Provide exceptional service to every guest."
+        },
         {
             "employer": "SCAD Media",
             "title": "Social Media Manager",
@@ -131,20 +131,20 @@ var work = {
             "dates": "2013-2015",
             "description": "Taught multiple sections of university level German 101, 102, 201, and 202 over four semesters. Responsible for all planning, instruction and assessment. Help make our language curriculum more awesome. Received top ratings on ratemyprofessors."
         },
-        // {
-        //     "employer": "Olive Garden",
-        //     "title": "Server",
-        //     "location": "Tucson, AZ",
-        //     "dates": "2013",
-        //     "description": "Give each guest a genuine, personalized Italian dining experience while also providing fast, efficient service."
-        // },
-        // {
-        //     "employer": "Jimmy John's Subs",
-        //     "title": "Delivery Driver",
-        //     "location": "Tucson, AZ",
-        //     "dates": "2013",
-        //     "description": "Make fast, efficient deliveries, interact with customers, accept payments, and make change. When not on delivery: clean, take phone orders, and ring up customers on the register."
-        // },
+        {
+            "employer": "Olive Garden",
+            "title": "Server",
+            "location": "Tucson, AZ",
+            "dates": "2013",
+            "description": "Give each guest a genuine, personalized Italian dining experience while also providing fast, efficient service."
+        },
+        {
+            "employer": "Jimmy John's Subs",
+            "title": "Delivery Driver",
+            "location": "Tucson, AZ",
+            "dates": "2013",
+            "description": "Make fast, efficient deliveries, interact with customers, accept payments, and make change. When not on delivery: clean, take phone orders, and ring up customers on the register."
+        },
         {
             "employer": "Intuit Inc.",
             "title": "Technical Support Representative",
@@ -152,20 +152,20 @@ var work = {
             "dates": "2012-2013",
             "description": "Delight customers by empathizing with customers and understanding their pain points. Play a crucial role in improving Intuit products."
         },
-        // {
-        //     "employer": "Petite Pea Catering on 6th",
-        //     "title": "Chef & Catering Assistant",
-        //     "location": "Tucson, AZ",
-        //     "dates": "2011",
-        //     "description": "Cook dishes for events. Discuss requirements with clients, create menus, prepare food, coordinate other cook staff, store food properly, and respect food safety standards."
-        // },
-        // {
-        //     "employer": "FUEL Modern Eatery",
-        //     "title": "Barista",
-        //     "location": "Tucson, AZ",
-        //     "dates": "2010-2012",
-        //     "description": "Prepare espresso beverages, smoothies, and frozen yogurt for members of Campus Recreation, students, and the general public. Clean. Accept payments and make change."
-        // },
+        {
+            "employer": "Petite Pea Catering on 6th",
+            "title": "Chef & Catering Assistant",
+            "location": "Tucson, AZ",
+            "dates": "2011",
+            "description": "Cook dishes for events. Discuss requirements with clients, create menus, prepare food, coordinate other cook staff, store food properly, and respect food safety standards."
+        },
+        {
+            "employer": "FUEL Modern Eatery",
+            "title": "Barista",
+            "location": "Tucson, AZ",
+            "dates": "2010-2012",
+            "description": "Prepare espresso beverages, smoothies, and frozen yogurt for members of Campus Recreation, students, and the general public. Clean. Accept payments and make change."
+        },
         {
             "employer": "University of Arizona Residence Life",
             "title": "Resident Assistant",
@@ -173,41 +173,41 @@ var work = {
             "dates": "2010-2012",
             "description": "Facilitate the social, academic, and personal adjustment of students to the residence halls and University. Develop a sense of community among residents as members of a floor, residents of a hall, and active participants in the residence life system."
         },
-        // {
-        //     "employer": "Target Corporation",
-        //     "title": "Jewelry & Intimates Specialist",
-        //     "location": "Tucson, AZ",
-        //     "dates": "Summer 2010",
-        //     "description": "Perform a variety of job duties throughout department store locations. Greet customers, stock shelves, organize displays, take inventory, and assist in sales. Serve as cashier."
-        // },
+        {
+            "employer": "Target Corporation",
+            "title": "Jewelry & Intimates Specialist",
+            "location": "Tucson, AZ",
+            "dates": "Summer 2010",
+            "description": "Perform a variety of job duties throughout department store locations. Greet customers, stock shelves, organize displays, take inventory, and assist in sales. Serve as cashier."
+        },
         {
             "employer": "University of Arizona Office of LGBTQ Affairs",
             "title": "Intern at ASUA Pride Alliance",
             "location": "Tucson, AZ",
             "dates": "2010",
             "description": ""
+        },
+        {
+            "employer": "Core Eatery",
+            "title": "Salad Maker",
+            "location": "Tucson, AZ",
+            "dates": "2009",
+            "description": "Prepared salads and rice bowls for members of the Campus community. Cleaned. Accept payments and make change."
+        },
+        {
+            "employer": "IHOP Restaurant",
+            "title": "Lead Server & Trainer",
+            "location": "Mesa, AZ",
+            "dates": "2005-2008",
+            "description": "Greet guests, take food and drink orders, serve all the dishes, and finalize sales transactions. Handle multiple situations, such as busy breakfast rushes, slower midday periods, and often-rambunctious late-night crowds."
+        },
+        {
+            "employer": "Petsmart",
+            "title": "Cashier",
+            "location": "Gilbert, AZ",
+            "dates": "2006",
+            "description": "Greet customers as they walk in, provide solutions and answers to their questions, and make sure they walk out with everything they need."
         }
-        // {
-        //     "employer": "Core Eatery",
-        //     "title": "Salad Maker",
-        //     "location": "Tucson, AZ",
-        //     "dates": "2009",
-        //     "description": "Prepared salads and rice bowls for members of the Campus community. Cleaned. Accept payments and make change."
-        // },
-        // {
-        //     "employer": "IHOP Restaurant",
-        //     "title": "Lead Server & Trainer",
-        //     "location": "Mesa, AZ",
-        //     "dates": "2005-2008",
-        //     "description": "Greet guests, take food and drink orders, serve all the dishes, and finalize sales transactions. Handle multiple situations, such as busy breakfast rushes, slower midday periods, and often-rambunctious late-night crowds."
-        // },
-        // {
-        //     "employer": "Petsmart",
-        //     "title": "Cashier",
-        //     "location": "Gilbert, AZ",
-        //     "dates": "2006",
-        //     "description": "Greet customers as they walk in, provide solutions and answers to their questions, and make sure they walk out with everything they need."
-        // }
     ], // End jobs
     display: function () {
         for (var job = 0; job < this.jobs.length; job++) {
@@ -228,27 +228,27 @@ var work = {
 var project = {
     "projects": [
         {
-            "title": "menumoo",
-            "dates": "2015-2016",
-            "description": "Developed a content management system using the Flask framework in Python. Authentication is provided via OAuth and all data is stored within a PostgreSQL database.",
-            "images": []
-        },
-        {
             "title": "dot-n-box",
             "dates": "2016",
             "description": "Implemented a scalable hangman game as an API with endpoints that allow anyone to develop a front-end for the game.",
             "images": []
         },
         {
-            "title": "all-play",
-            "dates": "2015",
-            "description": "Built a PostgreSQL relational database scheme to store the results of a game tournament. Also provided a number of queries to efficiently report the results of the tournament and determine the winner.",
+            "title": "menumoo-server-config",
+            "dates": "2016",
+            "description": "Installed and configured all required software to turn a baseline Ubuntu Amazon Web Services server into a fully functional web application server, including Apache Web Server and PostgreSQL database server.",
             "images": []
         },
         {
-            "title": "menumoo-server-config",
+            "title": "menumoo",
             "dates": "2015-2016",
-            "description": "Installed and configured all required software to turn a baseline Ubuntu Amazon Web Services server into a fully functional web application server, including Apache Web Server and PostgreSQL database server.",
+            "description": "Developed a content management system using the Flask framework in Python. Authentication is provided via OAuth and all data is stored within a PostgreSQL database.",
+            "images": []
+        },
+        {
+            "title": "all-play",
+            "dates": "2015",
+            "description": "Built a PostgreSQL relational database scheme to store the results of a game tournament. Also provided a number of queries to efficiently report the results of the tournament and determine the winner.",
             "images": []
         }
     ],
